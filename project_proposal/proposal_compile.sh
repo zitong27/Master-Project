80d503501b9e9455f3e3ad722da4f5da8f21a9b4
Zitong Zhao <zitong.zhao22@imperial.ac.uk>
2023-04-03 20:04:52 +0100

    proposal submit

---
+++ project_proposal/proposal_compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Desc: compiles LaTeX report
+
+
+pdflatex project_proposal.tex
+pdflatex project_proposal.tex
+pdflatex project_proposal.tex
+
+## Cleanup
+rm *.aux
+rm *.log
+rm *.bbl
+rm *.blg
+echo "Compiled LaTeX report"
